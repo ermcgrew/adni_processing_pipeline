ac927038e0203280196346c5b1363a0a0d57a41f
Emily McGrew <emcgrew@bscsub1.pmacs.upenn.edu>
2023-04-03 14:51:47 -0400

    function wmh prep

--- processing.py
+++ processing.py
@@ -1,5 +1,7 @@
+import datetime
 import logging
 import os
+
 
 ashs_t1_atlas = "/home/lxie/ASHS_atlases/PMC_3TT1_atlas_noSR"
 ashs_root = "/project/hippogang_2/longxie/pkg/ashs/ashs-fast"
@@ -8,9 +10,13 @@ icv_atlas = "/home/lxie/ASHS_atlases/ICVatlas_3TT1"
 ashs_t2_atlas = "/project/hippogang_2/pauly/wolk/atlases/ashs_atlas_upennpmc_20170810"
 sides = ["left", "right"]
 
-# adni_data_dir = "/project/wolk_2/ADNI2018/dataset/"
+adni_analysis_dir = "/project/wolk_2/ADNI2018/analysis_input"
+# adni_data_dir = "/project/wolk_2/ADNI2018/dataset"
 # for testing
-adni_data_dir = "/project/wolk_2/ADNI2018/scripts/pipeline_test_data/"
+adni_data_dir = "/project/wolk_2/ADNI2018/scripts/pipeline_test_data"
+
+current_date = datetime.datetime.now().strftime("%Y_%m_%d")
+
 
 def file_exists(filepath):
     return os.path.isfile(filepath) 
@@ -23,37 +29,37 @@ class MRI:
         self.id = subject
         self.mridate = mridate
         # self.T1_dicom_filepath = f"/project/wolk/PUBLIC/Dicoms/{self.id}/MRI3T/{self.mridate}/...dcm"
-        self.filepath=f"{adni_data_dir}{self.id}/{self.mridate}/"
+        self.filepath=f"{adni_data_dir}/{self.id}/{self.mridate}"
         self.date_id_prefix = f"{self.mridate}_{self.id}"
         
-        self.T1_nifti = f"{self.filepath}{self.date_id_prefix}_T1w.nii.gz"
-        self.T1_trim = f"{self.filepath}{self.date_id_prefix}_T1w_trim.nii.gz"
+        self.T1_nifti = f"{self.filepath}/{self.date_id_prefix}_T1w.nii.gz"
+        self.T1_trim = f"{self.filepath}/{self.date_id_prefix}_T1w_trim.nii.gz"
         
-        self.T1_extract_brain = f"{self.filepath}{self.date_id_prefix}_T1w_trim_brainx_ExtractedBrain.nii.gz"
-        self.T1_wb_seg= f"{self.filepath}{self.date_id_prefix}_wholebrainseg/{self.date_id_prefix}_T1w_trim_brainx_ExtractedBrain/{self.date_id_prefix}_T1w_trim_brainx_ExtractedBrain_wholebrainseg.nii.gz"
-        self.T1_wb_seg_QC = f"{self.filepath}{self.date_id_prefix}_wbseg_qa.png"
+        self.T1_extract_brain = f"{self.filepath}/{self.date_id_prefix}_T1w_trim_brainx_ExtractedBrain.nii.gz"
+        self.T1_wb_seg= f"{self.filepath}/{self.date_id_prefix}_wholebrainseg/{self.date_id_prefix}_T1w_trim_brainx_ExtractedBrain/{self.date_id_prefix}_T1w_trim_brainx_ExtractedBrain_wholebrainseg.nii.gz"
+        self.T1_wb_seg_QC = f"{self.filepath}/{self.date_id_prefix}_wbseg_qa.png"
         
-        self.T1_SR = f"{self.filepath}{self.date_id_prefix}_T1w_trim_denoised_SR.nii.gz"
+        self.T1_SR = f"{self.filepath}/{self.date_id_prefix}_T1w_trim_denoised_SR.nii.gz"
         
-        
-        self.T1_ashs_seg_left = f"{self.filepath}ASHST1/final/{self.id}_left_lfseg_heur.nii.gz"
-        self.T1_ashs_seg_right = f"{self.filepath}ASHST1/final/{self.id}_right_lfseg_heur.nii.gz"
-        self.T1_ashs_qc_left = f"{self.filepath}ASHST1/qa/qa_seg_bootstrap_heur_left_qa.png"
-        self.T1_ashs_qc_right = f"{self.filepath}ASHST1/qa/qa_seg_bootstrap_heur_right_qa.png"
-        self.T1_ashs_thick_left = f"{self.filepath}ASHST1_MTLCORTEX_MSTTHK/{self.date_id_prefix}_left_thickness.csv"
-        self.T1_ashs_thick_right = f"{self.filepath}ASHST1_MTLCORTEX_MSTTHK/{self.date_id_prefix}_right_thickness.csv"
-        self.T1_ashs_icv_qc_left = f"{self.filepath}ASHSICV/qa/qa_seg_multiatlas_corr_nogray_left_qa.png"
-        self.T1_ashs_icv_qc_right = f"{self.filepath}ASHSICV/qa/qa_seg_multiatlas_corr_nogray_right_qa.png"
+        self.T1_ashs_seg_left = f"{self.filepath}/ASHST1/final/{self.id}_left_lfseg_heur.nii.gz"
+        self.T1_ashs_seg_right = f"{self.filepath}/ASHST1/final/{self.id}_right_lfseg_heur.nii.gz"
+        self.T1_ashs_qc_left = f"{self.filepath}/ASHST1/qa/qa_seg_bootstrap_heur_left_qa.png"
+        self.T1_ashs_qc_right = f"{self.filepath}/ASHST1/qa/qa_seg_bootstrap_heur_right_qa.png"
+        self.T1_ashs_thick_left = f"{self.filepath}/ASHST1_MTLCORTEX_MSTTHK/{self.date_id_prefix}_left_thickness.csv"
+        self.T1_ashs_thick_right = f"{self.filepath}/ASHST1_MTLCORTEX_MSTTHK/{self.date_id_prefix}_right_thickness.csv"
+        self.T1_ashs_icv_qc_left = f"{self.filepath}/ASHSICV/qa/qa_seg_multiatlas_corr_nogray_left_qa.png"
+        self.T1_ashs_icv_qc_right = f"{self.filepath}/ASHSICV/qa/qa_seg_multiatlas_corr_nogray_right_qa.png"
 
-        self.T2_nifti = f"{self.filepath}{self.date_id_prefix}_T2w.nii.gz"
-        self.T2_ashs_seg_left = f"{self.filepath}sfsegnibtend/final/${self.id}_left_lfseg_corr_nogray.nii.gz"
-        self.T2_ashs_seg_right = f"{self.filepath}sfsegnibtend/final/${self.id}_right_lfseg_corr_nogray.nii.gz"
-        self.T2_ashs_qc_left = f"{self.filepath}sfsegnibtend/"
-        self.T2_ashs_qc_right = f"{self.filepath}sfsegnibtend/"
+        self.T2_nifti = f"{self.filepath}/{self.date_id_prefix}_T2w.nii.gz"
+        self.T2_ashs_seg_left = f"{self.filepath}/sfsegnibtend/final/${self.id}_left_lfseg_corr_nogray.nii.gz"
+        self.T2_ashs_seg_right = f"{self.filepath}/sfsegnibtend/final/${self.id}_right_lfseg_corr_nogray.nii.gz"
+        self.T2_ashs_qc_left = f"{self.filepath}/sfsegnibtend/"
+        self.T2_ashs_qc_right = f"{self.filepath}/sfsegnibtend/"
 
 
-        self.flair = f"{self.filepath}{self.date_id_prefix}_flair.nii.gz"
-        self.T1_flair = f"{self.filepath}{self.date_id_prefix}_T1w_trim_to_flair.mat"
+        self.flair = f"{self.filepath}/{self.date_id_prefix}_flair.nii.gz"
+        self.wmh = f"{self.filepath}/{self.date_id_prefix}_wmh.nii.gz"
+        self.T1_flair = f"{self.filepath}/{self.date_id_prefix}_T1w_trim_to_flair.mat"
 
     def ants_thick(self):
         print(f"Use FTDC's ANTS gear code")        
@@ -165,20 +171,32 @@ class MRI:
                 logging.info(f"{self.id}:{self.mridate}: Running T2 ASHS")
                 #export ashs_root in shell first
                 #export ASHS_ROOT=/project/hippogang_2/longxie/pkg/ashs/ashs-fast
-                os.system(f"mkdir {self.filepath}sfsegnibtend")
-                os.system(f"{ashs_root}/bin/ashs_main.sh \
-                      -a {ashs_t2_atlas} -g {self.T1_trim} -f {self.T2_nifti} \
-                      -d -T -I {self.id} -w {self.filepath}sfsegnibtend")
+                # os.system(f"mkdir {self.filepath}sfsegnibtend")
+                # os.system(f"{ashs_root}/bin/ashs_main.sh \
+                #       -a {ashs_t2_atlas} -g {self.T1_trim} -f {self.T2_nifti} \
+                #       -d -T -I {self.id} -w {self.filepath}sfsegnibtend")
             else:
                 logging.info(f"{self.id}:{self.mridate}: No T2 nifti, cannot run T2 ASHS.")
                 return
 
-
     def t1_flair_reg(self):
         print(f"bsub flirt {self.T1_trim} {self.flair}")
 
-    def wmh(self):
-        print(f"collect files to send to lambda-picsl {self.flair}")
+    def wmh_prep(self):
+        if file_exists(self.wmh):
+            logging.info(f"{self.id}:{self.mridate}: WMH analysis already run.")
+            return
+        else: 
+            if file_exists(self.flair):
+                if not os.path.exists(f"{adni_analysis_dir}/{current_date}"):
+                    logging.info(f"making directory {current_date} in analysis_input for WMH analysis.")               
+                    os.system(f"mkdir {adni_analysis_dir}/{current_date}")
+                
+                logging.info(f"{self.id}:{self.mridate}: collecting files to send to lambda-picsl")
+                os.system(f"cp {self.flair} {adni_analysis_dir}/{current_date}/{self.mridate}_{self.id}_flair_0000.nii.gz")
+            else:
+                logging.info(f"{self.id}:{self.mridate}: No flair nifti, cannot run WMH analysis.")
+                return
 
 
 class AmyloidPET:
@@ -223,20 +241,24 @@ class T1PetReg:
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
+
+
+
 MRIprocessing=MRI('141_S_6779','2020-10-27')
 
 # print(MRIprocessing.T1_nifti)
 # print(MRIprocessing.T1_trim)
 # print(MRIprocessing.T1_extract_brain)
 # print(MRIprocessing.T1_SR)
-print(MRIprocessing.T1_ashs_seg_left)
+# print(MRIprocessing.T1_ashs_seg_left)
 
 # MRIprocessing.wb_seg()
 # MRIprocessing.t1_super_res()
 # MRIprocessing.t1_ashs()
 # MRIprocessing.t1_ashs_multitemplate_thickness()
-MRIprocessing.t2_ashs()
-
+# MRIprocessing.t2_ashs()
+# MRIprocessing.t1_flair_reg()
+MRIprocessing.wmh_prep()
 
 
 # Amyloidprocessing = AmyloidPET("035_S_6788","2019-06-13")
