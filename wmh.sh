c2cbf74893ec0245b2d31f65360408a0ec150ec8
ermcgrew <ermcgrew@gmail.com>
2023-04-03 16:04:32 -0400

    wmh.sh for moving files to lambda-picsl for processing and back to session folders

--- wmh.sh
+++ wmh.sh
@@ -1,55 +1,90 @@
 #!/usr/bin/bash
 
+Help()
+{
+    echo "For use with ADNI files only."
+    echo "Get commands to copy flair files to lambda-picsl for white matter hyperintensity analysis" 
+    echo "and move output files back to bscid or copy output files to session folders."
+    echo 
+    echo "Options:"
+    echo "-c     Copy output files to session folders."
+    echo "-d     directory name in /project/wolk_2/ADNI2018/analysis_input/ that contains both data_for_inference/ and output_from_nnunet_inference/."
+    echo "-g     Print commands to copy files and run docker container to the terminal"
+    echo "-h     Print this help."
+    echo "-u     Your bscsub/chead/lambda-picsl username."
+    
+    echo Example usages: 
+    echo "bash wmh.sh -d 2023_04_03 -u emcgrew -g"
+    echo "bash wmh.sh -d 2023_04_03 -c"
+    }
 
-#Usage:
-## -c flag and enter username, current date to print out commands for copying files to lambda-picsl
-    #and running docker container
+Get_commands()
+{
+echo Printing commands for username $username and directory $directory.
+echo "Format is @clustername:working/directory$"
+echo "      command to copy and run"
 
-# -m flag and enter current date to copy wmh files to correct session folders
+echo "ONE"
+echo "@scisub$"
+echo "      scp -r $username@bscsub.pmacs.upenn.edu:/project/wolk_2/ADNI2018/analysis_input/$directory/data_for_inference $username@chead.uphs.upenn.edu:/home/$username"
 
-'''
-@scisub$$ 
-    scp -r <username>@bscsub.pmacs.upenn.edu:/project/wolk_2/ADNI2018/analysis_input/{current_date}_wmh_input/ \
-    <username>@chead.uphs.upenn.edu:/home/<username>
+echo "TWO"
+echo "@lambda-picsl:/data/$username$"
+echo "      scp -r $username@chead.uphs.upenn.edu:/home/$username/data_for_inference/ ." 
 
-@lambda-picsl:/data/<username>$$
-    scp -r <username>@chead.uphs.upenn.edu:/home/<username>/{current_date}_wmh_input/ . 
+echo "THREE"
+echo "@lambda-picsl$"
+echo "      docker pull pulks/docker_hippogang_exvivo_segm:v1.3.0"
 
-@lambda-picsl$$
-    docker pull pulks/docker_hippogang_exvivo_segm:v1.3.0
+echo "FOUR"
+echo "@lambda-picsl$"
+echo "      docker run --gpus all --privileged -v /data/$username/:/data/exvivo/ -it pulks/docker_hippogang_exvivo_segm:v1.3.0 /bin/bash -c "bash /src/commands_nnunet_inference_WMH_invivo.sh" >> logs.txt"
 
-@lambda-picsl$$
-    docker run --gpus all --privileged -v /data/<username>/:/data/exvivo/ \
-    -it pulks/docker_hippogang_exvivo_segm:v1.3.0 \
-    /bin/bash -c "bash /src/commands_nnunet_inference_WMH_invivo.sh" >> logs.txt
+echo "FIVE"
+echo "@lambda-picsl:/data/$username/data_for_inference$"
+echo "      scp -r ./output_from_nnunet_inference $username@chead.uphs.upenn.edu:/home/$username/"
 
-@lambda-picsl:/data/<username>/data_for_inference$$
-    scp -r ./output_from_nnunet_inference <username>@chead.uphs.upenn.edu:/home/<username>/
-
-@chead:~$$
-    scp -r ./output_from_nnunet_inference \
-    <username>@bscsub.pmacs.upenn.edu:/project/wolk_2/ADNI2018/analysis_input/
-
-
-'''
-
+echo "SIX"
+echo "@chead:~$"
+echo "      scp -r ./output_from_nnunet_inference $username@bscsub.pmacs.upenn.edu:/project/wolk_2/ADNI2018/analysis_input/$directory"
+}
 
 
+Copy_outputs(){
+echo Copying WMH outputs from $directory directory to session folders
 
-#move files produced from docker container on lambda to correct session folders
-output_directory="/project/wolk_2/ADNI2018/analysis_input/{current_date}"
+output_directory="/project/wolk_2/ADNI2018/analysis_input/$directory"
 dataset="/project/wolk_2/ADNI2018/dataset"
 for f in $output_directory/output_from_nnunet_inference/*.gz ; do
     filename=$(basename $f)
-    ##check file names to make sure cut gets correct field
-    sub=$(echo $filename | cut -d '_' -f 2 )
+    id=$(echo $filename | cut -d '_' -f 2 )
     date=$(echo $filename | cut -d '_' -f 1 )
-  fi
 
-  if [[ -d ${dataset}/${sub}/${date}/ ]] ; then
+  if [[ -d ${dataset}/${id}/${date}/ ]] ; then
     echo copying file $f
-    cp $f ${dataset}/${sub}/${date}/${date}_${sub}_wmh.nii.gz
+    cp $f ${dataset}/${id}/${date}/${date}_${id}_wmh.nii.gz
   else
-    echo folder for session $sub/$date does not exist yet
+    echo folder for session $id/$date does not exist yet
   fi
+done
+}
+
+# Get the options
+while getopts "cd:hgu:" option; do
+    case $option in
+        h) # display Help
+            Help
+            exit;;
+        d) 
+            directory=$OPTARG;;
+        u) 
+            username=$OPTARG;;
+        g) 
+            Get_commands;;    
+        c) 
+            Copy_outputs;;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            exit;;
+    esac
 done
