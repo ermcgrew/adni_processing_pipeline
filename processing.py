916f45fa6b01f8afc5f6e99b4bb12180ed30d4e1
Emily McGrew <emcgrew@bscsub1.pmacs.upenn.edu>
2023-04-05 16:06:47 -0400

    bsub -w options

--- processing.py
+++ processing.py
@@ -23,8 +23,30 @@ def file_exists(filepath):
     # works for files and symlinks
     # to check for directories too, use os.path.exists
 
+def wait_value(parent_job_name):
+    if parent_job_name:
+        return f"-w done({parent_job_name})"
+    else:
+        return ""
+
+def run_status(id,date,input_file,output_file,processing_step):
+    if file_exists(output_file):
+        logging.info(f"{id}:{date}: {processing_step} already run.")
+        return False
+    else:
+        if file_exists(input_file):
+            logging.info(f"{id}:{date}: Running {processing_step}")
+            return True
+        else:
+            logging.info(f"{id}:{date}: No input file, cannot run {processing_step}")
+            return False
+
+if run_status('3452','2020-11-11','file','file', 'brainx'):
+    print("we should run this")
 
 class MRI:
+    #strings for MRI filepaths and functions for MRI processing
+
     def __init__(self, subject, mridate):
         self.id = subject
         self.mridate = mridate
@@ -61,16 +83,21 @@ class MRI:
         self.T1_flair = f"{self.filepath}/{self.date_id_prefix}_T1w_trim_to_flair.mat"
 
     def ants_thick(self):
-        print(f"Use FTDC's ANTS gear code")        
+        print(f"Use FTDC's ANTS gear code")
+        this_job_name=f"ants_{self.date_id_prefix}"
+        
     
-    def wb_seg(self):
+    def wb_seg(self, parent_job_name = ""):
+        wait = wait_value(parent_job_name)
+        this_job_name=f"brainx_{self.date_id_prefix}"
         if file_exists(self.T1_wb_seg):
-            logging.info(f"Whole Brain Segmentation already done for {self.T1_nifti}")
+            logging.info(f"{self.id}:{self.mridate}: Whole Brain Segmentation already done.")
             return
         else:
             if file_exists(self.T1_trim):
                 logging.info(f"{self.id}:{self.mridate}: Running whole brain extraction")
                 # os.system(f'bsub -o {self.filepath} ./wrapper_scripts/brain_extract.sh {self.T1_trim}')
+                
                 logging.info(f"{self.id}:{self.mridate}: Running whole brain segmentation")
                 # os.system(f"bsub -o {self.filepath} -M 12G -q bsc_long \
                 #       /home/sudas/bin/ahead_joint/turnkey/bin/hippo_seg_WholeBrain_itkv4_v3.sh \
@@ -78,7 +105,7 @@ class MRI:
                 #       {self.filepath}{self.date_id_prefix}_wholebrainseg \
                 #       {self.date_id_prefix}_T1w_trim_brainx_ExtractedBrain \
                 #       /home/sudas/bin/ahead_joint/turnkey/data/WholeBrain_brainonly 1")
-                ###need to wait for wbseg file to be generated before running this piece:
+                
                 if os.path.isfile(self.T1_wb_seg_QC):
                     logging.info(f"{self.id}:{self.mridate}: Whole brain segmentation QC file already generated")
                     return
@@ -92,7 +119,9 @@ class MRI:
                 logging.info(f"{self.id}:{self.mridate}: No T1 trim file, cannot run whole brain segmentation")
                 return
     
-    def t1_super_res(self):
+    def t1_super_res(self, parent_job_name = ""):
+        wait = wait_value(parent_job_name)
+        this_job_name=f"superres_{self.date_id_prefix}"
         if file_exists(self.T1_SR):
             logging.info(f"{self.id}:{self.mridate}: Super Resolution already created")
             return
@@ -105,7 +134,9 @@ class MRI:
                 logging.info(f"{self.id}:{self.mridate}: No T1 trim file, cannot run super resolution")
                 return
 
-    def t1_ashs(self):
+    def t1_ashs(self, parent_job_name = ""):
+        wait = wait_value(parent_job_name)
+        this_job_name=f"t1ashs_{self.date_id_prefix}"
         if file_exists(self.T1_ashs_qc_left) or file_exists(self.T1_ashs_qc_right):
             logging.info(f"{self.id}:{self.mridate}: ASHST1 already run")
             return
@@ -113,6 +144,17 @@ class MRI:
             if file_exists(self.T1_trim) and file_exists(self.T1_SR):
                 logging.info(f"{self.id}:{self.mridate}: Running ASHST1")
                 # os.system(f"mkdir {self.filepath}ASHST1")
+                ## set ASHS_ROOT before running
+                os.system(f"bsub -J {this_job_name} {wait} -o {self.filepath} \
+                      {ashs_root}/bin/ashs_main.sh \
+                      -a {ashs_t1_atlas} -d -T -I {self.id} -g {self.T1_trim} -f {self.T1_SR} \
+                      -l -s 1-7 \
+                      -z {long_scripts}/ashs-fast-z.sh \
+                      -m {long_scripts}/identity.mat -M \
+                      -w {self.filepath}ASHST1")
+                
+                return(this_job_name)
+
                 # os.system(f"{ashs_root}/bin/ashs_main.sh \
                 #     -a {ashs_t1_atlas} -d -T -I {self.id} -g {self.T1_trim} -f {self.T1_SR} \
                 #     -l -s 1-7 \
@@ -123,7 +165,38 @@ class MRI:
                 logging.info(f"{self.id}:{self.mridate}: No T1 trim file or no T1_denoised_SR file, cannot run ASHST1")
                 return
     
-    def t1_ashs_icv(self):
+
+    def t1_ashs_multitemplate_thickness(self, parent_job_name = ""):
+        wait = wait_value(parent_job_name)
+        for side in sides:
+            if side == "left":
+                ashs_thick = self.T1_ashs_thick_left
+                ashs_seg = self.T1_ashs_seg_left
+                this_job_name=f"t1mtthk_left_{self.date_id_prefix}"
+            elif side == "right":
+                ashs_thick = self.T1_ashs_thick_right
+                ashs_seg = self.T1_ashs_seg_right
+                this_job_name=f"t1mtthk_right_{self.date_id_prefix}"
+
+            if file_exists(ashs_thick):
+                logging.info(f"{self.id}:{self.mridate}: Multi Template Thickness {side} already run")
+            else:
+                if file_exists(ashs_seg):
+                    logging.info(f"{self.id}:{self.mridate}: Running Multi Template Thickness {side}")
+                    os.system(f"bsub -o {self.filepath} -J {this_job_name} {wait} -M 12G -n 1 \
+                               ./wrapper_scripts/multitemplate_thickness.sh {self.id} {self.mridate}\
+                               {side} {ashs_seg} {self.filepath}ASHST1_MTLCORTEX_MSTTHK")
+
+                    # os.system(f"bsub -o {self.filepath} -M 12G -n 1 \
+                    #           ./wrapper_scripts/multitemplate_thickness.sh {self.id} {self.mridate}\
+                    #           {side} {ashs_seg} {self.filepath}ASHST1_MTLCORTEX_MSTTHK")    
+                else:
+                    logging.info(f"{self.id}:{self.mridate}: No ASHS segmentation file, cannot run Multi Template Thickness {side}")
+
+
+    def t1_ashs_icv(self, parent_job_name = ""):
+        wait = wait_value(parent_job_name)
+        this_job_name=f"t1icv_{self.date_id_prefix}"
         if file_exists(self.T1_ashs_icv_qc_left) or file_exists(self.T1_ashs_icv_qc_right):
             logging.info(f"{self.id}:{self.mridate}: ASHSICV already run")
             return
@@ -140,29 +213,11 @@ class MRI:
                 #     -w {self.filepath} &")
             else:
                 logging.info(f"{self.id}:{self.mridate}: No T1 trim file, cannot run ASHSICV")
-                return
+                return   
 
-    def t1_ashs_multitemplate_thickness(self):
-        for side in sides:
-            if side == "left":
-                ashs_thick = self.T1_ashs_thick_left
-                ashs_seg = self.T1_ashs_seg_left
-            elif side == "right":
-                ashs_thick = self.T1_ashs_thick_right
-                ashs_seg = self.T1_ashs_seg_right
-
-            if file_exists(ashs_thick):
-                logging.info(f"{self.id}:{self.mridate}: Multi Template Thickness {side} already run")
-            else:
-                if file_exists(ashs_seg):
-                    logging.info(f"{self.id}:{self.mridate}: Running Multi Template Thickness {side}")
-                    # os.system(f"bsub -o {self.filepath} -M 12G -n 1 \
-                    #           ./wrapper_scripts/multitemplate_thickness.sh {self.id} {self.mridate}\
-                    #           {side} {ashs_seg} {self.filepath}ASHST1_MTLCORTEX_MSTTHK")    
-                else:
-                    logging.info(f"{self.id}:{self.mridate}: No ASHS segmentation file, cannot run Multi Template Thickness {side}")
-
-    def t2_ashs(self):
+    def t2_ashs(self, parent_job_name = ""):
+        wait = wait_value(parent_job_name)
+        this_job_name=f"t2ashs_{self.date_id_prefix}"
         if file_exists(self.T2_ashs_seg_left) or file_exists(self.T2_ashs_seg_right):
             logging.info(f"{self.id}:{self.mridate}: T2 ASHS already run")
             return
@@ -179,14 +234,18 @@ class MRI:
                 logging.info(f"{self.id}:{self.mridate}: No T2 nifti, cannot run T2 ASHS.")
                 return
 
-    def t1_flair_reg(self):
+    def t1_flair_reg(self, parent_job_name = ""):
+        wait = wait_value(parent_job_name)
+        this_job_name=f"t1flair_{self.date_id_prefix}"
         if file_exists(self.T1_flair):
             logging.info(f"{self.id}:{self.mridate}: T1 to Flair registration already run.")
             return
         else: 
             if file_exists(self.flair) and file_exists(self.T1_trim):               
                 logging.info(f"{self.id}:{self.mridate}: Running T1 to Flair registration")
-                # os.system(f"bsub -o {self.filepath} ./wrapper_scripts/t1_flair_reg.sh {self.T1_trim} {self.flair} {self.T1_flair}")
+                os.system(f"bsub -o {self.filepath} -J {this_job_name} {wait} \
+                          ./wrapper_scripts/t1_flair_reg.sh {self.T1_trim} {self.flair} {self.T1_flair}")
+                return(this_job_name)
             else:
                 logging.info(f"{self.id}:{self.mridate}: No flair nifti or no T1 trim nifti, cannot run T1 to Flair registration.")
                 return
@@ -201,14 +260,15 @@ class MRI:
                     logging.info(f"making directory {current_date} in analysis_input for WMH analysis.")               
                     os.system(f"mkdir {adni_analysis_dir}/{current_date}")
                 
-                logging.info(f"{self.id}:{self.mridate}: collecting files to send to lambda-picsl")
-                # os.system(f"cp {self.flair} {adni_analysis_dir}/{current_date}/{self.mridate}_{self.id}_flair_0000.nii.gz")
+                logging.info(f"{self.id}:{self.mridate}: collectinng flair files to send to lambda-picsl")
+                os.system(f"cp {self.flair} {adni_analysis_dir}/{current_date}/{self.mridate}_{self.id}_flair_0000.nii.gz")
             else:
                 logging.info(f"{self.id}:{self.mridate}: No flair nifti, cannot run WMH analysis.")
                 return
 
 
 class AmyloidPET:
+    # strings for Amyloid PET filepaths
     def __init__(self, subject, amydate):
         self.id = subject
         self.amydate = amydate
@@ -218,6 +278,7 @@ class AmyloidPET:
 
 
 class TauPET:
+    # strings for Tau PET filepaths
     def __init__(self, subject, taudate):
         self.id = subject
         self.taudate = taudate
@@ -227,6 +288,7 @@ class TauPET:
         
 
 class T1PetReg:
+    #strings for filepaths from MRI and PET class instances and function to do T1-PET registration
     def __init__(self, pet_type, T1, PET):
         self.id = T1.id
         self.mridate = T1.mridate
@@ -283,13 +345,14 @@ MRIprocessing=MRI('141_S_6779','2020-10-27')
 
 # MRIprocessing.wb_seg()
 # MRIprocessing.t1_super_res()
-# MRIprocessing.t1_ashs()
-# MRIprocessing.t1_ashs_multitemplate_thickness()
+# jobname=MRIprocessing.t1_ashs()
+# os.system(f"bjobs -J {jobname}")
+# MRIprocessing.t1_ashs_multitemplate_thickness("t1_ashs_2020-10-27_141_S_6779")
 # MRIprocessing.t2_ashs()
 # MRIprocessing.t1_flair_reg()
 # MRIprocessing.wmh_prep()
 
 Amyloidprocessing = AmyloidPET("141_S_6779","2020-11-11")
 testreg = T1PetReg('amypet', MRIprocessing, Amyloidprocessing)
-print(testreg.reg_RAS)
-testreg.pet_registration()
+# print(testreg.reg_RAS)
+# testreg.pet_registration()
