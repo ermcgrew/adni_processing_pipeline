ff60a4639b6e43df13ea00e8bfeef966bbd50f4d
ermcgrew <ermcgrew@gmail.com>
2023-04-19 15:29:20 -0400

    testing wbseg

--- wrapper_scripts/multitemplate_thickness.sh
+++ wrapper_scripts/multitemplate_thickness.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/bash
-#Usage: multitemplate_thickness.sh subjectID scanDate side ASHS_segmentation_heur output_directory
-#adapted from /home/lxie/ADNI2018/scripts/RunT1BaselineLongitudinal_09092022.sh
+# #Usage: multitemplate_thickness.sh subjectID scanDate side ASHS_segmentation_heur output_directory
+# #adapted from /home/lxie/ADNI2018/scripts/RunT1BaselineLongitudinal_09092022.sh
+
+# id=$1
+# date=$2
+# side=$3
+# seg=$4
+# output_directory=$5
+
+# module load matlab
+# module load vtk/7.1.1
+
+# mkdir -p $output_directory
+# /home/lxie/ASHS_T1/pipeline_package/MultiTempThkMTLWithHippo/runpipeline_MultiAndUnifiTemplate_single.sh \
+#     ${id}_${date} $seg $side $output_directory 
+
+# rm -rf $output_directory/data $output_directory/RegToAtlases $output_directory/RegToInitTemp $output_directory/RegToUT
+
+
 
 id=$1
 PREFIX=$2
