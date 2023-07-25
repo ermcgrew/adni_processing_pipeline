3ce6abbc8409a1b2da698d4306296ae3004dfed5
ermcgrew <ermcgrew@gmail.com>
2023-07-25 09:43:09 -0400

    csv variables to pass to create_pet and id_new functions

--- config.py
+++ config.py
@@ -26,11 +26,11 @@ registry_csv = [file for file in original_ida_datasheets if "REGISTRY" in file][
 csvs_mri_merge = [file for file in cleaned_ida_datasheets if "MRI3META" in file or "MRILIST" in file]
 pet_meta_list = [file for file in cleaned_ida_datasheets if 'PET_META_LIST' in file][0]
 
-
 # merged data csv names, join with datasetup_directories_path["merged_data_uids"]
 mri_uids = "mri_uids.csv"
 # mri_uids = "mri_uids_smalltest.csv"
 pet_uid_csv_list = ["fdg_uids.csv","amy_uids.csv","tau_uids.csv"]
+tau_anchored_csv = "tau_anchored_uids.csv"
 
 #processing status csv names, join with datasetup_directories_path["uids_process_status"]
 mri_uids_processing = "mri_uids_processing_status.csv"
@@ -44,23 +44,11 @@ tau_filelocations  = "tau_filelocations.csv"
 amy_filelocations  = "amy_filelocations.csv"
 fdg_filelocations  = "fdg_filelocationss.csv"
 
+
 ##get previous run's filepath files for comparison to new uids
-##TODO: use uids csv instead of filepath one?
-# fileloc_directory_previousrun_basename = [x for x in adni_data_csvs_directories_allruns[4:8] if "fileloc" in x][0]
-# fileloc_directory_previousrun = os.path.join(adni_data_setup_csvs_directory,fileloc_directory_previousrun_basename)
-# previous_filelocs_csvs = os.listdir(fileloc_directory_previousrun)
-# previous_mri_filelocs = [x for x in previous_filelocs_csvs if "MRI" in x][0]
-
-
-# mergeduids_directory_previousrun_basename = [x for x in adni_data_csvs_directories_allruns[4:8] if "merge" in x][0]
-# mergeduids_directory_previousrun=os.path.join(adni_data_setup_csvs_directory,mergeduids_directory_previousrun_basename)
-
-# scantype_dictionary = {"MRI":"","AMY":"","TAU":"","FDG":""}
-# for key in scantype_dictionary:
-#    current_uid_csv = [x for x in os.listdir(datasetup_directories_path["merged_data_uids"]) if key.casefold() in x or key in x]
-#    previous_uid_csv = [x for x in os.listdir(mergeduids_directory_previousrun) if key.casefold() in x or key in x]
-#    scantype_dictionary[key] = current_uid_csv + previous_uid_csv
-   
+fileloc_directory_previousrun_basename = [x for x in adni_data_csvs_directories_allruns[4:8] if "fileloc" in x][0]
+fileloc_directory_previousrun = os.path.join(adni_data_setup_csvs_directory,fileloc_directory_previousrun_basename)
+previous_filelocs_csvs = os.listdir(fileloc_directory_previousrun)
 
 
    
