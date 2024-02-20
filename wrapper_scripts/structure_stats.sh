7fadad1d99b6b55a9d12f7bd531cc662350088e1
Emily McGrew <emcgrew@bscsub1.pmacs.upenn.edu>
2024-02-20 12:19:07 -0500

    split stats function in structure and pet stats functions

--- wrapper_scripts/structure_stats.sh
+++ wrapper_scripts/structure_stats.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/bash
 
-id=$1  
-wholebrainseg=$2 
-thickness=$3 
-wblabelfile=$4
-mridate=$5
+wholebrainseg=$1 
+thickness=$2
+wblabelfile=$3
+outputfile=$4
+
+TMPDIR=$(mktemp -d)
 
 # Mask with GM
 tissueseg=$(echo $thickness | sed -e 's/CorticalThickness/BrainSegmentation/g')
-# All ROIs thickness
 MASKCOMM="$tissueseg -interp NN -reslice-identity -thresh 2 2 1 0 -times"
-c3d $wholebrainseg -dup $MASKCOMM -as A $thickness -interp NN -reslice-identity -push A  -lstat > $TMPDIR/allthick.txt
+c3d $wholebrainseg -dup $MASKCOMM -as A $thickness -interp NN -reslice-identity -push A -lstat > $TMPDIR/allthick.txt
+
+# All ROIs thickness
 for i in $(cat $wblabelfile | grep -v '#' | sed -n '9,$p' | \
     grep -v -E 'vessel|Chiasm|Caudate|Putamen|Stem|White|Accumb|Cerebell|subcallo|Vent|allidum|CSF' | awk '{print $1}' ); do    
         THISTHICK=$(cat $TMPDIR/allthick.txt | sed -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/' | grep "^$i " | awk '{print $2}')
         statline="$statline\t $THISTHICK"
 done
 
-echo -e $statline >> ${stats_output_dir}/stats_mri_${mridate}_${id}_structure.txt
+echo -e $statline >> $outputfile
+
+rm -r $TMPDIR
