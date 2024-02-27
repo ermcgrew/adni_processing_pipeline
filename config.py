2a82d74e3d6468c07add1e93ee091f6b08d2f733
Emily McGrew <emcgrew@bscsub1.pmacs.upenn.edu>
2024-02-27 15:16:40 -0500

    new headers in create_stats, braak stages use wbseg to ants in stats

--- config.py
+++ config.py
@@ -72,12 +72,13 @@ ashs_root = "/project/hippogang_2/pauly/wolk/ashs-fast"
 
 
 ###Steps for argparse choices
-##Order matters; values match MRI.method & MRIPetReg.method names
+##Order matters: steps are ordered so dependent processing steps come after their parent processing step
+##Names matter: values match MRI.method & MRIPetReg.method names
 ##for naming: if whole_brain_seg was called "wbseg", it matches to "wbseg_to_ants" and "wbsegqc" as well
 mri_processing_steps = ["neck_trim", "ants", "brain_ex", "whole_brain_seg", "wbseg_to_ants", "wbsegqc", "inf_cereb_mask", 
-                        "t1icv", "superres","t1ashs", "t1mtthk","t2ashs", "t2ashs_qconly", "prc_cleanup", 
-                        "wmh", "pmtau", "ashst1_stats", "ashst2_stats", "structpetstats"]
-registration_steps = ["t1_pet_reg", "tau_suvr", "tau_pvc", "t2_pet_reg", "pet_reg_qc", "structpetstats"]
+                        "t1icv", "superres","t1ashs", "t1mtthk", "t2ashs", "t2ashs_qconly", "prc_cleanup", 
+                        "wmh", "pmtau", "ashst1_stats", "ashst2_stats", "structure_stats", "pet_stats"]
+registration_steps = ["t1_pet_reg", "tau_suvr", "tau_pvc", "t2_pet_reg", "pet_reg_qc", "pet_stats"]
 
 
 ###Data sheets & derived csvs names and locations
--- create_stats_sheets.sh
+++ create_stats_sheets.sh
@@ -2,70 +2,112 @@
 
 function write_header()
 {
-  HEADER="RID\tID\tMRIDATE\tAMYDATE\tTAUDATE\tICV\tSlice_Thickness"
-  list=$(echo CA1 CA2 CA3 DG MISC SUB ERC BA35 BA36 PHC sulcus CA HIPP EXTHIPPO EXTHIPPOno36 MTLno36)
-  for side in left right; do
-    for sf in $list; do
-      HEADER="${HEADER}\t${side}_${sf}_vol\t${side}_${sf}_ns\t${side}_${sf}_tau\t${side}_${sf}_amy"
+  mode=$1
+  statfile=$2
+  
+  HEADER="RID,ID,MRIDATE,"
+
+  if [[ $mode == "pet_stats" ]] ; then 
+    HEADER="${HEADER}\tAMYDATE\tTAUDATE\tICV\tSlice_Thickness"
+
+    ##ASHST2 hippocampal ROIs
+    list=$(echo CA1 CA2 CA3 DG MISC SUB ERC BA35 BA36 PHC sulcus CA HIPP EXTHIPPO EXTHIPPOno36 MTLno36)
+    for side in left right; do
+      for sf in $list; do
+        HEADER="${HEADER}\t${side}_${sf}_tau\t${side}_${sf}_amy"
+      done
     done
-  done
 
-  HEADER="$HEADER\tHIPPboth_tau\tHIPPboth_amy\tEXTHIPPno36both_tau\tEXTHIPPno36both_amy\t\
-  MTLno36both_tau\tMTLno36both_amy\tcereb_tau\tcereb_amy\tocc_tau\tocc_amy\tpostcing_tau\tpostcing_amy\t\
-  inftemp_tau_R\tinftemp_amy_R\tinftemp_tau_L\tinftemp_amy_L\tmidtemp_tau_R\tmidtemp_amy_R\tmidtemp_tau_L\t\
-  midtemp_amy_L\tsuptemp_tau_R\tsuptemp_amy_R\tsuptemp_tau_L\tsuptemp_amy_L\tsuppariet_tau_R\t\
-  suppariet_amy_R\tsuppariet_tau_L\tsuppariet_amy_L\tcalc_tau_R\tcalc_amy_R\tcalc_tau_L\tcalc_amy_L\t\
-  angular_tau_R\tangular_amy_R\tangular_tau_L\tangular_amy_L\tsupfront_tau_R\tsupfront_amy_R\t\
-  supfront_tau_L\tsupfront_amy_L"
+    ##Combined hippocampal ROIs and CEREB tau and amy values
+    HEADER="$HEADER\tHIPPboth_tau\tHIPPboth_amy\tEXTHIPPno36both_tau\tEXTHIPPno36both_amy\t\
+    MTLno36both_tau\tMTLno36both_amy\tcereb_tau\tcereb_amy"
+
+    #Whole brain ROIS: tau, taupvc, amy
+    HEADER="$HEADER\tocc_tau\tocc_tau_pvc\tocc_amy\tpostcing_tau\tpostcing_tau_pvc\tpostcing_amy"
+    wbrois=$(echo inftemp midtemp suptemp suppariet calc angular supfront)
+    for roi in wbrois ; do 
+      for side in R L ; do
+        HEADER="${HEADER}\t${roi}_tau_${side}\t${roi}_tau_pvc_${side}\t${roi}_amy_${side}"
+      done 
+    done
+
+    for roi in $(cat $wblabels | grep -v '#' | sed -n '9,$p' \
+    | grep -v -E 'vessel|Chiasm|Caudate|Putamen|Stem|White|Accumb|Cerebell|subcallo|Vent|allidum|CSF' \
+    | cut -f 2 -d \" | sed -e 's/\( \)\{1,\}/_/g' ); do
+      HEADER="$HEADER\t${roi}_tau\t${roi}_taupvc\t${roi}_amy"
+    done
+
+    #compSUVR
+    HEADER="${HEADER}\tcompSUVR"
+
+    #BRAAK stages
+    for wbtype in WB_ants WB ; do 
+      for tautype in tau tau_suvr ; do 
+        for side in L R; do
+          for region in BRAAK3 BRAAK4 BRAAK5 BRAAK6 ; do
+          HEADER="${HEADER}\t${side}_${region}_${tautype}_${wbtype}"
+    done
+    done
+    done
+    done 
+
+  elif [[ $mode == "structure" ]] ; then 
+    for roi in $(cat $wblabels | grep -v '#' | sed -n '9,$p' \
+      | grep -v -E 'vessel|Chiasm|Caudate|Putamen|Stem|White|Accumb|Cerebell|subcallo|Vent|allidum|CSF' \
+      | cut -f 2 -d \" | sed -e 's/\( \)\{1,\}/_/g' ); do
+      HEADER="$HEADER\t${roi}_thickness"
+    done
+
+  elif [[ $mode == "ashst1" ]] ; then 
+    HEADER="ICV_ASHSICV"
+
+    # ASHS T1 baseline volume and thickness
+    for type in VOL; do
+    for side in L R M; do
+    for sub in AHippo PHippo ERC BA35 BA36 PHC; do
+      HEADER="$HEADER,${side}_${sub}_${type}_ASHST1_3T"
+    done
+    done
+    done
+
+    # thickness
+    for side in L R M; do
+    for type in MeanTHK MedianTHK; do
+    for sub in Hippo ERC BA35 BA36 PHC; do
+      HEADER="$HEADER,${side}_${sub}_${type}_MSTTHKMT_ASHST1_3T"
+    done
+    done
+    done
+
+    # thickness quality of fit
+    for side in L R; do
+    for sub in Hippo ERC BA35 BA36 PHC ALL; do
+      HEADER="$HEADER,${side}_${sub}_${type}_MSTFitQuality_ASHST1_3T"
+    done
+    done
   
-  for roi in $(cat $wblabels | grep -v '#' | sed -n '9,$p' \
-  | grep -v -E 'vessel|Chiasm|Caudate|Putamen|Stem|White|Accumb|Cerebell|subcallo|Vent|allidum|CSF' \
-  | cut -f 2 -d \" | sed -e 's/\( \)\{1,\}/_/g' ); do
-    HEADER="$HEADER\t${roi}_tau\t${roi}_amy\t${roi}_thickness"
-  done
+    #PMTAU
+    for i in Anterior Posterior; do
+    HEADER="$HEADER\t${i}_pmtauthick\t${i}_pmtauweightedthick\t${i}_pmtaujac\t${i}_pmtauweightedjac"
+    done
 
-  #WMH
-  HEADER="$HEADER\tWMH_vol"
-  
-  #PMTAU
-  for i in Anterior Posterior; do
-  HEADER="$HEADER\t${i}_pmtauthick\t${i}_pmtauweightedthick\t${i}_pmtaujac\t${i}_pmtauweightedjac"
-  done
-  
-  echo -e "${HEADER}" > $this_run_analysis_output_dir/$tsvfilename
-  echo -e "${HEADER}" > $this_run_analysis_output_dir/$structonly
+  elif [[ $mode == "ASHST2" ]] ; then 
+    list=$(echo CA1 CA2 CA3 DG MISC SUB ERC BA35 BA36 PHC sulcus CA HIPP EXTHIPPO EXTHIPPOno36 MTLno36)
+    for side in left right; do
+      for sf in $list; do
+        HEADER="${HEADER}\t${side}_${sf}_vol\t${side}_${sf}_ns\t${side}_${sf}_tau\t${side}_${sf}_amy"
+      done
+    done
+    #PMTAU
+    for i in Anterior Posterior; do
+    HEADER="$HEADER\t${i}_pmtauthick\t${i}_pmtauweightedthick\t${i}_pmtaujac\t${i}_pmtauweightedjac"
+    done
 
+  elif [[ $mode == "wmh" ]] ; then 
+    HEADER="$HEADER\tWMH_vol"
+  fi 
 
-  ##########################################
-  #for ashst1 & mtthk header
-  ashsheader="RID,ID,MRIDATE,ICV_ASHSICV"
-
-  # ASHS T1 baseline volume and thickness
-  for type in VOL; do
-  for side in L R M; do
-  for sub in AHippo PHippo ERC BA35 BA36 PHC; do
-    ashsheader="$ashsheader,${side}_${sub}_${type}_ASHST1_3T"
-  done
-  done
-  done
-
-  # thickness
-  for side in L R M; do
-  for type in MeanTHK MedianTHK; do
-  for sub in Hippo ERC BA35 BA36 PHC; do
-    ashsheader="$ashsheader,${side}_${sub}_${type}_MSTTHKMT_ASHST1_3T"
-  done
-  done
-  done
-
-  # thickness quality of fit
-  for side in L R; do
-  for sub in Hippo ERC BA35 BA36 PHC ALL; do
-    ashsheader="$ashsheader,${side}_${sub}_${type}_MSTFitQuality_ASHST1_3T"
-  done
-  done
-
-  echo -e "${ashsheader}" > $this_run_analysis_output_dir/$ashst1csv
+  echo -e "${HEADER}" > $statfile
 
 }
 
@@ -86,12 +128,15 @@ function collate_new_data ()
 wblabels=$1
 stats_output_dir=$2
 this_run_analysis_output_dir=$3
+##mode as argument? 
 
 date=$(echo $this_run_analysis_output_dir | cut -f 6 -d "/")
+
+#by mode: statfile =
 tsvfilename="pet_ashst2_stats_corr_nogray_${date}.tsv"
 ashst1csv="ASHS_T1MTTHK_${date}.csv"
 structonly="structonly_ashst2_stats_corr_nogray_${date}.tsv"
 
 # echo "Writing tsv header"
-write_header
+write_header($mode, $statfile)
 collate_new_data
