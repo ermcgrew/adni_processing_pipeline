b63d4a60b1b57e2889844073a4c6494e765cfb5f
Emily McGrew <emcgrew@bscsub1.pmacs.upenn.edu>
2024-06-13 09:54:39 -0400

    download dicom script for local machine added to repo

--- queue_watch.sh
+++ queue_watch.sh
@@ -3,10 +3,8 @@
 sleep 5 #for jobs to start processing
 ALLJOBS=$(bjobs | grep "RUN" | wc -l)
 while [ $ALLJOBS -gt 1 ]; do  #don't include this running script
-    # echo $ALLJOBS still running
     sleep 60
     ALLJOBS=$(bjobs | grep "RUN" | wc -l)
 done
-
 
 echo "all processing and stats creation jobs finished"
